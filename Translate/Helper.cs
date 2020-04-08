822d27887deaf16d78881e99be29aa66fecb47ad
mishafominih <53509997+mishafominih@users.noreply.github.com>
2020-04-08 13:45:08 +0500

    Добавил класс Helper и тесты к нему
    содежит некоторые функции, которые могут пригодиться в будущем

---
+++ Translate/Helper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Translate
+{
+	public class Helper
+	{
+		private static List<string> NameFiles = new List<string>();
+
+		public static IEnumerable<string> SplitOnWords(string text)
+		{
+			foreach (var word in text.Split(new[] {
+				' ', ',', '.', '\'', '"', ':', ';', '!', '?' },
+				StringSplitOptions.RemoveEmptyEntries))
+			{
+				yield return word;
+			}
+		}
+
+		public static IEnumerable<string> SplitOnSentence(string text)
+		{
+			foreach (var sentence in text.Split(new[] { '.', '!', '?' },
+				StringSplitOptions.RemoveEmptyEntries)
+				.Select(x => (x[0] == ' ') ? x.Remove(0, 1) : x))
+			{
+				yield return sentence;
+			}
+		}
+
+		public static IEnumerable<string> GetNameFiles()
+		{
+			foreach (var name in NameFiles)
+				yield return name;
+		}
+
+		public static void SaveText(string nameFile, string text)
+		{
+			if (!NameFiles.Contains(nameFile)) NameFiles.Add(nameFile);
+			File.WriteAllText("texts\\" + nameFile, text);
+		}
+
+		public static string GetText(string nameFile)
+		{
+			return File.ReadAllText("texts\\" + nameFile);
+		}
+	}
+}
---
+++ Translate/HelperTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace Translate
+{
+	[TestFixture]
+	class Tests
+	{
+		[TestCase("Hello, world", new string[] { "Hello", "world" })]
+		public void TestSplitOnWords(string Start, string[] expected)
+		{
+			Assert.AreEqual(expected, Helper.SplitOnWords(Start));
+		}
+
+		[TestCase("Hello, my friend. World, i love you", new string[] { "Hello, my friend", "World, i love you" })]
+		public void TestSplitOnSentence(string Start, string[] expected)
+		{
+			Assert.AreEqual(expected, Helper.SplitOnSentence(Start));
+		}
+
+		[Test]
+		public void TestSaveText()
+		{
+			Helper.SaveText("test.txt", "hello");
+			Assert.AreEqual(1, Helper.GetNameFiles().Count());
+			Assert.AreEqual("hello", Helper.GetText("test.txt"));
+			Helper.SaveText("test.txt", "world");
+			Assert.AreEqual(1, Helper.GetNameFiles().Count());
+			Assert.AreEqual("world", Helper.GetText("test.txt"));
+		}
+	}
+}
