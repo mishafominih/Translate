a4277638f0152a021718b5b6dd0c6be5b85f1f91
yura159 <47238316+yura159@users.noreply.github.com>
2020-04-25 20:47:33 +0500

    Класс, который переводит текст
    Добавил класс, который должен будет переводить текст. Пока словарь не реализован, текст еще не переводится.

---
+++ Translate/TranslateText.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Translate
+{
+    public class TranslateText
+    {
+        public static string[] Text { get; private set; }
+
+        public TranslateText(string text)
+        {
+            Text = text.Split(' ');
+        }
+
+        // Перевод самого текста 
+        public string GetTextTranslation()
+        {
+            var result = new StringBuilder();
+            result.Append(GetSentenceTranslation(Text[0]));
+            for(var i = 1; i < Text.Length; i++)
+            {
+                var sentence = GetSentenceTranslation(Text[i]);
+                result.Append(' ' + sentence);
+            }
+            return result.ToString();
+        }
+
+        // Переводим предложения 
+        private static string GetSentenceTranslation(string sentence)
+        {
+            string newSentence;
+            var len = sentence.Length - 1;
+            if (!Char.IsLetter(sentence, len))
+                return newSentence = GetWordTranslation(sentence.Remove(len)) + sentence[len];
+            return newSentence = GetWordTranslation(sentence);
+        }
+
+        // Тут мы будем переводить слова, обращаясь к словарю.
+        private static string GetWordTranslation(string sentence)
+        {
+            return sentence;
+        }
+    }
+}
---
+++ TranslateTest/TranslateTextTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Translate;
+
+namespace TranslateTest
+{
+    [TestClass]
+    public class TranslateTextTest
+    {
+        [TestMethod]
+        public void TranslateTest()
+        {
+            var text = new TranslateText("Шел снег, я бежал по улице. Моя собака наблюдала за падающими снежинками.");
+            var newText = text.GetTextTranslation();
+            Assert.AreEqual("Шел снег, я бежал по улице. Моя собака наблюдала за падающими снежинками.", newText);
+        }
+    }
+}
