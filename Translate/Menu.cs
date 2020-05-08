d9bbabf65a77eff064173729d01f2941f0f9b82f
yura159 <47238316+yura159@users.noreply.github.com>
2020-05-09 01:03:31 +0500

    Добавил стартовое окно

---
+++ Translate/Menu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Translate
+{
+	public class Menu : Form
+	{
+		public Menu()
+		{
+			ClientSize = new Size(1280, 720);
+			BackgroundImage = Image.FromFile(new DirectoryInfo("Images") + @"\Menu.jpg");
+			Initialize();
+		}
+
+		private void Initialize()
+		{
+			var lite = new Button();
+			var pro = new Button();
+			CreateButton(lite, new Rectangle(361, 485, 241, 49), "Lite");
+			CreateButton(pro, new Rectangle(676, 485, 241, 49), "Pro");
+		}
+
+		private void CreateButton(Button button, Rectangle rec, string text)
+		{
+			button.Bounds = rec;
+			button.Text = text;
+			button.BackColor = Color.FromArgb(138, 154, 169);
+			//button.Font = new Font("Merienda One");
+			Controls.Add(button);
+		}
+	}
+}
--- Translate/Program.cs
+++ Translate/Program.cs
@@ -14,7 +14,7 @@ namespace Translate
 		[STAThread]
 		static void Main()
 		{
-			Application.Run(new Form1());
+			Application.Run(new Menu());
 		}
 	}
 }
