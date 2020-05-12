d6cdda37680afd81cc377ec9e1fb484de671150b
yura159 <47238316+yura159@users.noreply.github.com>
2020-05-12 18:08:58 +0500

    Button and Font
    Основыными действиями было:

---
+++ Translate/Forms/ProTarnslate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Translate
+{
+    public partial class ProTarnslate : Form
+    {
+        public ProTarnslate()
+        {
+            ClientSize = new Size(1280, 720);
+            BackgroundImage = Image.FromFile(new DirectoryInfo("Images") + @"\ProTranslate.jpg");
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            var inputTextBox = new TextBox();
+            var outputTextBox = new TextBox();
+            CreateTextBox(inputTextBox, 99, 148);
+            CreateTextBox(outputTextBox, 559, 148);
+        }
+
+        private void CreateTextBox(TextBox textBox, int x, int y)
+        {
+            textBox.Multiline = true;
+            textBox.BackColor = Color.FromArgb(236, 236, 236);
+            textBox.Location = new Point(x, y);
+            textBox.Size = new Size(449, 480);
+            Controls.Add(textBox);
+        }
+    }
+}
